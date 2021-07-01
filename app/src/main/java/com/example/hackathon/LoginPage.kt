d07fcd440ed57336319d752ee4ec65fce66bc10a
Anirban001js <67429520+Anirban001js@users.noreply.github.com>
2021-07-01 21:06:15 +0530

    Kotlin file for login_page.xml

---
+++ app/src/main/java/com/example/hackathon/LoginPage.kt
@@ -0,0 +1,10 @@
+package com.example.hackathon
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginPage : AppCompatActivity() {
+
+}
--- app/src/main/java/com/example/hackathon/MainActivity.kt
+++ app/src/main/java/com/example/hackathon/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.hackathon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
-        println("Hello World!!")
+    }
+    fun signUp(view: View)
+    {
+        setContentView(R.layout.activity_main)
+    }
+    fun signIn(view: View)
+    {
+        setContentView(R.layout.activity_main)
+    }
+    fun skip(view: View)
+    {
+        setContentView(R.layout.activity_main)
     }
 }
